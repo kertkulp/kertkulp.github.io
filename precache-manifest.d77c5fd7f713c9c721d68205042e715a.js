6127af1c41130aa23505e788da11a7c3c6808a7c
Kert Kulp <kertkulp4@gmail.com>
2020-08-25 17:44:47 +0300

    Commercial page example

---
+++ precache-manifest.d77c5fd7f713c9c721d68205042e715a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a0a36545b2362068b7e1fab6f5f61c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aa92b7d547dcc00a82c",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "ea66781a1541c4d0bb06",
+    "url": "/static/css/main.2186314f.chunk.css"
+  },
+  {
+    "revision": "3aa92b7d547dcc00a82c",
+    "url": "/static/js/2.09756f1c.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.09756f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea66781a1541c4d0bb06",
+    "url": "/static/js/main.58d6a3e8.chunk.js"
+  },
+  {
+    "revision": "774c39b3799cca80890b",
+    "url": "/static/js/runtime-main.77c3c7c1.js"
+  },
+  {
+    "revision": "ffa8a0bb6d6219cb2c6026a845f249bb",
+    "url": "/static/media/background1.ffa8a0bb.jpg"
+  },
+  {
+    "revision": "660f3abfcc0d2cf9d5956bb396e774c9",
+    "url": "/static/media/background2.660f3abf.jpg"
+  },
+  {
+    "revision": "2dae6d7e74decfdb796ad51f774e5a17",
+    "url": "/static/media/background3.2dae6d7e.jpg"
+  }
+]);
